9daaa611ad94402eddbca9072bb99b54b637a343
Syamsundar M <SM33977@TechMahindra.com>
2017-09-01 11:53:00 +0530

    Checking for Issue

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 public static void main(String args[]) {
 
 for(int i=0;i<10;i++) {
-	System.out.println(" Updating Hello World Project "+i);
+	System.out.println(" Verifying for Compilation Issues "+j);
 	}
 
 }
