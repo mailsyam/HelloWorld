b52500c70810edafc49331ab06d19e8c16bf0ed9
Syamsundar M <SM33977@TechMahindra.com>
2017-09-01 11:49:08 +0530

    Modified to verify Mail Option

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 public static void main(String args[]) {
 
 for(int i=0;i<10;i++) {
-	System.out.println(" Hello World Update "+i);
+	System.out.println(" Updating Hello World Project "+i);
 	}
 
 }
